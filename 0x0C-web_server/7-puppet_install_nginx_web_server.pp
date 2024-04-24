a1df1d536761068d179cddeb59c2850c2612da4a
menaujiri <ujirimena@gmail.com>
2024-04-23 21:27:41 -0700

    redirect

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,22 +1,21 @@
-# Install and configure nginx
-package { 'jfryman-nginx':
-  ensure => installed,
+# Setup nginx server
+
+package { 'nginx':
+  ensure     => 'installed',
 }
 
-include nginx
-
-class { 'nginx':
-  manage_repo    => true,
-  package_source => 'nginx-stable',
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
 }
 
-nginx::resource::server { '34.73.76.135':
-  listen_port      => 80,
-  www_root         => '/var/www/html/',
-  vhost_cfg_append => { 'rewrite' => '^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent' },
+file_line { 'aaaaa':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
-file { 'index':
-  path    => '/var/www/html/index.nginx-debian.html',
-  content => 'Holberton School for the win!',
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
 }
